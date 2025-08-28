6543bf7ff87f249c2aa23628eb98d3a5dcd5d3ef
ElangoRaja7 <elangorajece2k10@gmail.com>
2025-08-28 12:54:34 +0530

    mult slot ng-content did not work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
 
 import { BlogListComponent } from './blog-list/blog-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BlogListComponent],
+  imports: [RouterOutlet,BlogListComponent,ShellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
